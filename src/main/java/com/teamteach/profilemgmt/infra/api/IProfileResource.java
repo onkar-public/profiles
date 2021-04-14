7eac7d7ce26e80f06572a71546f61962a22cf292
Onkar Parmar <onkar@OnkarMac.local>
2021-04-14 09:23:16 +0530

    resource name for api gateway

--- src/main/java/com/teamteach/profilemgmt/infra/api/IProfileResource.java
+++ src/main/java/com/teamteach/profilemgmt/infra/api/IProfileResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Collections;
 
-@RequestMapping("")
+@RequestMapping("api")
 public interface IProfileResource {
 
     @PostMapping("create")
