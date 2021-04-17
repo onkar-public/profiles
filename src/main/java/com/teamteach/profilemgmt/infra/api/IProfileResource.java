3d694c2708754eacb6a1cd8ceab7bc155ed019ff
EC2 Default User <ec2-user@ip-172-31-13-69.ap-south-1.compute.internal>
2021-04-17 11:22:29 +0530

    api gateway docs

--- src/main/java/com/teamteach/profilemgmt/infra/api/IProfileResource.java
+++ src/main/java/com/teamteach/profilemgmt/infra/api/IProfileResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Collections;
 
-@RequestMapping("server")
+@RequestMapping("profiles")
 public interface IProfileResource {
 
     @PostMapping("create")
