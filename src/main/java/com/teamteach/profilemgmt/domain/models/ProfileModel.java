f2f4f235c32eb4bf0fef577645819b6e6b14da87
Onkar Parmar <onkar@OnkarMac.local>
2021-04-19 13:07:05 +0530

    custom id

--- src/main/java/com/teamteach/profilemgmt/domain/models/ProfileModel.java
+++ src/main/java/com/teamteach/profilemgmt/domain/models/ProfileModel.java
@@ -3,6 +3,7 @@ package com.teamteach.profilemgmt.domain.models;
 import com.teamteach.profilemgmt.domain.models.vo.IndividualType;
 import com.teamteach.profilemgmt.domain.models.vo.ProfileImage;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 
 import lombok.Data;
@@ -18,14 +19,15 @@ public class ProfileModel {
     @Transient
     public static final String SEQUENCE_NAME = "profiles_sequence";
 
-    String userId;
-    String ownerId;
-    String info;
-    String birthYear;
-    String fName;
-    String lName;
-    IndividualType userType;
-    ProfileImage profileImage;
+    @Id
+    protected String userId;
+    private String ownerId;
+    private String info;
+    private String birthYear;
+    private String fName;
+    private String lName;
+    private IndividualType userType;
+    private ProfileImage profileImage;
 
     public void setProfileImage(ProfileImage profileImage) {
         this.profileImage = profileImage;
